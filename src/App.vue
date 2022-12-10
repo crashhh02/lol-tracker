85e6a2476ee02875b3f224558541dd4e7e4ed30f
christian <christian.palladini2@gmail.com>
2022-12-10 17:00:36 +0100

    Commit

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,29 @@ import Sidebar from '@/components/Sidebar/Sidebar.vue'
 </script>
 
 <template>
+  <Sidebar/>
   <RouterView />
 </template>
 
-<style scoped>
+<style >
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+#app {
+  display: flex;
+}
 
+main {
+  flex: 1 1 0;
+  padding: 2rem;
+}
+
+@media (max-width: 768px) {
+  main {
+    padding-left: 6rem;
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,9 @@
 </script>
 
 <template>
-
+    <main>
+        <div class="bg-red-200">
+            <h1 class="text-red-500">Home</h1>
+        </div> 
+    </main>       
 </template>
